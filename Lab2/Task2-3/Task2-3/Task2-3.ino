b4a414ff1eff31bd612ab922b8d511a137a50075
joanna0420 <joanna.ee13@nycu.edu.tw>
2026-09-24 10:27:24 +0800

    2-3

--- Lab2/Task2-3/Task2-3/Task2-3.ino
+++ Lab2/Task2-3/Task2-3/Task2-3.ino
@@ -5,7 +5,6 @@ const int ledPinB = 9;
 volatile bool ledStateA = false;
 bool ledStateB = false;
 bool lastButtonB = HIGH;
-
 void setup() {
   pinMode(ledPinA, OUTPUT);
   pinMode(buttonPinA, INPUT_PULLUP);
@@ -19,13 +18,12 @@ void buttonISR() {
   ledStateA = !ledStateA;
   digitalWrite(ledPinA, ledStateA);
 }
-
 void loop() {
   bool currentButtonB = digitalRead(buttonPinB);//Arduino主動去看ButtonB現在是HIGH還是LOW
-  if (lastButtonB == HIGH && currentButtonB == LOW) {// 偵測HIGH→LOW也就是按下按鈕的瞬間
+  if (lastButtonB == HIGH && currentButtonB == LOW) {//偵測HIGH→LOW也就是按下按鈕的瞬間
     ledStateB = !ledStateB;
     digitalWrite(ledPinB, ledStateB);
   }
   lastButtonB = currentButtonB;
-  delay(2000);//2秒內Arduino根本沒有去檢查Button B
+  delay(2000);
 }
